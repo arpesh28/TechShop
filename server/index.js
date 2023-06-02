09cf7b010c2cbf1e9d16927446e843f1c5ad11ae
arpesh28 <arpeshgadekar@gmail.com>
2023-06-02 10:45:42 +0530

    build commands

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import connectToDatabase from "./database.js";
 import path from "path";
 
@@ -8,24 +9,27 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
-dotenv.config();
 connectToDatabase();
 const app = express();
 
 app.use(express.json());
-
-const port = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
-
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening to ${port}...`);
